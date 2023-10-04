76e2ea7131bb6a8462b0889ce14cf347a2e694fb
sifat7252 <safikulislam.sifat@gmail.com>
2023-10-04 23:51:56 +0600

    right side navigation part added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -11,14 +11,14 @@ const Home = () => {
       <BreakingNews></BreakingNews>
       <Navbar></Navbar>
       
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-6 mt-3">
         <div className="left-side border  ">
             <LeftSideNav></LeftSideNav>
         </div>
         <div className="middle border md:col-span-2">
             <h2 className="text-2xl">News is coming soon!!!!</h2>
         </div>
-        <div className="right-side border">
+        <div className="right-side ">
             <RightSideNav></RightSideNav>
         </div>
       </div>
--- src/Pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/Pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,9 +1,26 @@
-
+import { FaGithub, FaGoogle, FaFacebook, FaTwitter, FaInstagram,  } from 'react-icons/fa';
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2 className="text-2xl">Right side nav</h2>
+            <div className="p-4">
+                <h2 className="text-2xl text-black font-bold">Log In With</h2>
+                <br />
+                <button className='btn btn-outline flex text-sky-500 w-full m-2 mx-auto'><FaGoogle></FaGoogle> Google LogIn</button>
+                <button className='btn btn-outline flex text-black-500 w-full m-2 mx-auto'><FaGithub></FaGithub> Github LogIn</button>
+            </div>
+            <div className="p-4">
+            <h2 className="text-2xl text-black font-bold">Find Us On</h2>
+            <br />
+            <a className='flex w-full mx-auto border justify-center items-center font-medium text-base py-2 gap-2' href=""><FaFacebook></FaFacebook> Facebook</a>
+            <a className='flex w-full mx-auto border justify-center items-center font-medium text-base py-2 gap-2' href=""><FaTwitter></FaTwitter> Twitter</a>
+            <a className='flex w-full mx-auto border justify-center items-center font-medium text-base py-2 gap-2' href=""><FaInstagram></FaInstagram> Instagram</a>
+
+            </div>
+            <div className='bg-slate-300 p-4'>
+                <h2 className="text-xl font-semibold text-black">Q-Zone</h2>
+
+            </div>
         </div>
     );
 };
